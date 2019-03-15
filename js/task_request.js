b8064f275be9423c66fe9e392fca3cf0bd450667
Julien <lineproex@gmail.com>
2019-03-15 12:00:42 +0100

    Further jQuery removal
    Is a WIP for the people request

--- js/task_request.js
+++ js/task_request.js
@@ -1,52 +1,52 @@
 // This script fires once you load the page, no event listeners here!
-var dateID;
-var contentID;
+let dateID;
+let contentID;
 
-var request = new XMLHttpRequest();
-request.open('GET', 'php/tasks_request.php', true);
+let requesttask = new XMLHttpRequest();
+requesttask.open('GET', 'php/tasks_request.php', true);
 
-request.onload = function() {
-    if (request.status >= 200 && request.status < 400) {
+requesttask.onload = function() {
+    if (requesttask.status >= 200 && requesttask.status < 400) {
         // Success!
-        var data = JSON.parse(request.responseText);
+        let data = JSON.parse(requesttask.responseText);
         data.forEach(function(item, i) {
             dateID = "date-" + item.id;
             contentID = "task-content-" + item.id;
 
-            var tr = document.createElement("tr");
-            var tdpeople = document.createElement("td");
-                tdpeople.classList.add("task-people-id");
-                tdpeople.setAttribute("id", item.peopleid);
-                tdpeople.innerHTML = item.name;
-            tr.appendChild(tdpeople);
+            let tr = document.createElement("tr");
+                let tdpeople = document.createElement("td");
+                    tdpeople.classList.add("task-people-id");
+                    tdpeople.setAttribute("id", item.peopleid);
+                    tdpeople.innerHTML = item.name;
+                tr.appendChild(tdpeople);
 
-            var tddate = document.createElement("td");
-                tddate.setAttribute("id", dateID);
-                tddate.innerHTML = item.date;
-            tr.appendChild(tddate);
+                let tddate = document.createElement("td");
+                    tddate.setAttribute("id", dateID);
+                    tddate.innerHTML = item.date;
+                tr.appendChild(tddate);
 
-            var tdtask = document.createElement("td");
-                tdtask.setAttribute("id", contentID);
-                tdtask.innerHTML = item.content;
-            tr.appendChild(tdtask);                
+                let tdtask = document.createElement("td");
+                    tdtask.setAttribute("id", contentID);
+                    tdtask.innerHTML = item.content;
+                tr.appendChild(tdtask);                
 
-            var tdactions = document.createElement("td");
-                var btnedit = document.createElement("button");
-                    btnedit.classList.add("btn-edit");
-                    btnedit.setAttribute("id", "task-btn-edit");
-                    btnedit.value = item.id;
-                tdactions.appendChild(btnedit);
+                let tdactions = document.createElement("td");
+                    let btnedit = document.createElement("button");
+                        btnedit.classList.add("btn-edit");
+                        btnedit.setAttribute("id", "task-btn-edit");
+                        btnedit.value = item.id;
+                    tdactions.appendChild(btnedit);
 
-                var btndel = document.createElement("button");
-                    btndel.classList.add("btn-delete");
-                    btndel.setAttribute("id", "task-btn-delete");
-                    btndel.value = item.id;
-                tdactions.appendChild(btndel);
-            tr.appendChild(tdactions);
+                    let btndel = document.createElement("button");
+                        btndel.classList.add("btn-delete");
+                        btndel.setAttribute("id", "task-btn-delete");
+                        btndel.value = item.id;
+                    tdactions.appendChild(btndel);
+                tr.appendChild(tdactions);
 
             document.getElementById("tasks-table").appendChild(tr);
         });
     }
 };
 
-request.send();
+requesttask.send();
